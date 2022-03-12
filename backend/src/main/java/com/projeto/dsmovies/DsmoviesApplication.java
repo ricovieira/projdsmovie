a0f904baf5cc9d4b527e51c4a01e4ffe6cf99edf
Ricardo Vieira <ricovieira26@gmail.com>
2022-03-12 15:14:45 -0300

    Projeto criado

---
+++ backend/src/main/java/com/projeto/dsmovies/DsmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.dsmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projeto/dsmovies/DsmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.dsmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
