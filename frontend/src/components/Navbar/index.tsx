ff30733d754442911ac8ec623b7067bf754a8088
Ricardo Vieira <ricovieira26@gmail.com>
2022-03-12 16:36:40 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIcon } from 'assets/img/git.svg'
+import './styles.css'
+
+function Navbar(){
+    return(
+        <header>
+    <nav className="container">
+        <div className="dsmovie-nav-content">
+            <h1>Projeto DSMovie</h1>
+            <a href="https://github.com/ricovieira" target="_blank" rel="noreferrer">
+                <div className="dsmovie-contact-container">
+                    <GithubIcon />
+                    <p className="dsmovie-contact-link">/ricovieira</p>
+                </div>
+            </a>
+        </div>
+    </nav>
+</header>
+    );
+}
+
+export default Navbar;
