d677ac9b2cdea2f99600e54397d4e782a6cc1f0d
Ricardo Vieira <ricovieira26@gmail.com>
2022-03-13 10:47:28 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,14 @@
+import axios from "axios";
 import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+        });
 
     return (
         <>
